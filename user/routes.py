71e2ac5b6871d0e8fddc8c7e5ff2578678d81aea
Monirul Islam <mmislam027@gmail.com>
2025-12-13 21:25:12 +0600

    fix(user): implement UserService with proper dependencis injections

--- user/routes.py
+++ user/routes.py
@@ -1,37 +1,32 @@
-from fastapi import APIRouter
-from fastapi import Depends, HTTPException
-from h11 import Data
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from core.dependencies import auth
+
+from core.dependencies.auth import auth
 from database.database import db_instance
+from user.dto.login import LoginDto
+from user.dto.registration import RegistrationDto
 from user.dto.user import UserResponseDto
 from user.models.user import User
-from user.dto.login import LoginDto, LoginResponse
-from user.dto.registration import RegistrationDto
-from user.service import create_new_user, get_user_by_email, user_login
-from core.dependencies.auth import auth
+from user.service import UserService
 
 router = APIRouter()
 
 
+def get_user_service(db: Session = Depends(db_instance.get_db)) -> UserService:
+    return UserService(db)
+
+
 @router.post("/login")
-def login(data: LoginDto, db: Session = Depends(db_instance.get_db)):
-    return user_login(db, data)
+def login(data: LoginDto, user_service: UserService = Depends(get_user_service)):
+    return user_service.user_login(data)
 
 
 @router.post("/register")
 def register(
-    data: RegistrationDto,
-    db: Session = Depends(db_instance.get_db),
+    data: RegistrationDto, user_service: UserService = Depends(get_user_service)
 ):
-    db_user = get_user_by_email(db, data.email)
-    if db_user:
-        raise HTTPException(
-            status_code=400,
-            detail="Email is already exist",
-        )
 
-    new_user = create_new_user(db, data)
+    new_user = user_service.create_new_user(data)
     return {
         "message": "user registered successfully",
         "user_id": new_user.id,
@@ -39,8 +34,5 @@ def register(
 
 
 @router.get("/me", response_model=UserResponseDto)
-def me(
-    db: Session = Depends(db_instance.get_db),
-    current_user: User = Depends(auth.get_current_user),
-):
+def me(current_user: User = Depends(auth.get_current_user)):
     return current_user
--- user/service.py
+++ user/service.py
@@ -1,52 +1,55 @@
-import email
-from token import tok_name
-from tokenize import TokenError
 from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
-from core.token import access_token
+
 from core.security.hash import Hash
+from core.token import access_token
 from user.dto.login import LoginDto
 from user.dto.registration import RegistrationDto
 from user.models.user import User
 
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
-
-
-def create_new_user(db: Session, data: RegistrationDto):
-
-    hash_password = Hash.bcrypt(data.password)
-    new_user = User(
-        email=data.email,
-        password=hash_password,
+class UserService:
+    invalid_user_exception = HTTPException(
+        status_code=400,
+        detail="Invalid user credentials",
     )
-    db.add(new_user)
-    db.commit()
 
-    db.refresh(new_user)
-    return new_user
+    def __init__(self, db: Session) -> None:
+        self.db = db
 
+    def get_user_by_email(self, email: str):
+        stmt = select(User).where(User.email == email)
+        return self.db.scalars(stmt).first()
 
-def user_login(db: Session, data: LoginDto):
-    user = get_user_by_email(db, data.email)
-    if not user:
-        raise HTTPException(
-            status_code=400,
-            detail="Invalid user credentials",
+    def create_new_user(self, data: RegistrationDto):
+
+        if self.get_user_by_email(data.email):
+            raise HTTPException(
+                status_code=400,
+                detail="Email is already exist",
+            )
+
+        hash_password = Hash.bcrypt(data.password)
+        new_user = User(
+            email=data.email,
+            password=hash_password,
         )
-    is_matched = Hash.verify(data.password, user.password)
-    if not is_matched:
-        raise HTTPException(
-            status_code=400,
-            detail="Invalid user credentials",
-        )
-    token = access_token(
-        {
-            "sub": user.email,
+        self.db.add(new_user)
+        self.db.commit()
+
+        self.db.refresh(new_user)
+        return new_user
+
+    def user_login(self, data: LoginDto):
+        user = self.get_user_by_email(data.email)
+        if not user:
+            raise self.invalid_user_exception
+        is_matched = Hash.verify(data.password, user.password)
+        if not is_matched:
+            raise self.invalid_user_exception
+        token = access_token({"sub": user.email})
+        return {
+            "access_token": token,
+            "token_type": "bearer",
         }
-    )
-    return {
-        "token": token,
-        "token_type": "bearer",
-    }
